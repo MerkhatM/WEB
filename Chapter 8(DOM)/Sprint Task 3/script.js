82a78b5482f356f4a3786ee359516019795bdba4
MerkhatM <merhatmanlaev@gmail.com>
2023-06-07 13:08:06 +0600

    Sprint Task 3

--- Chapter 8(DOM)/Sprint Task 3/script.js
+++ Chapter 8(DOM)/Sprint Task 3/script.js
@@ -1,11 +1,27 @@
 let select=document.getElementById("selected_id")
-let value=select.value;
 let money=document.getElementById("money")
 let kzt=document.getElementById("kzt")
 let eur=document.getElementById("eur")
 let usd=document.getElementById("usd")
-if(value=="KZT"){
-    usd.value=parseInt(money.value*407);
-    eur.value=parseInt(money.value*504);
-    kzt.value="---"
+
+function showResult() {
+    if(select.value==="KZT"){
+        kzt.value="---"
+        usd.value=(money.value/431).toFixed(2);
+        eur.value=(money.value/509).toFixed(2);
+
+    }
+    else if(select.value==="USD"){
+        usd.value="---"
+        eur.value=(money.value*429/509).toFixed(2)
+        kzt.value=(money.value*429).toFixed(2)
+    }
+    else if(select.value==="EUR"){
+        eur.value="---"
+        usd.value=(money.value*507/431).toFixed(2)
+        kzt.value=(money.value*507).toFixed(2)
+    }
 }
+
+money.addEventListener("keyup",showResult)
+select.addEventListener("change",showResult)
